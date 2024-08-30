36ca5699e719928569b1f0eced0b06ac897fb5f7
Somik9 <weblife999@gmail.com>
2024-08-30 13:18:50 +0500

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.guessNum = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessNum = Math.round((this.min + this.max) / 2);
+        return this.guessNum;
     }
 
     lower() {
-
-    }
+        this.max = this.guessNum;
+        }
+    
 
     greater() {
-
+        this.min = this.guessNum;
     }
 }
 
